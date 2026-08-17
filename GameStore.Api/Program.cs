499622614bcbbe09c43467ee2b49ff6231b129b0
maherhms <maher.3000@outlook.fr>
2026-08-17 10:21:40 +0200

    Implement Api calls

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,4 +1,7 @@
-using GameStore.Api;
+using GameStore.Api.Dtos;
+
+const string GetGameEndpointName = "GameGame";
+
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -28,7 +31,49 @@ List<GameDto> games = [
 
 // GET /games
 app.MapGet("/games", () => games);
+
 // GET /games/id
-app.MapGet("games/{id}" , (int id) => games.Find(game => game.Id == id));
+app.MapGet("/games/{id}" , (int id) =>
+{
+    games.Find(game => game.Id == id);
+}).WithName(GetGameEndpointName);
+
+// POST /games
+app.MapPost("/games", (CreateGameDto newGame) =>
+{
+    GameDto game = new (
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+    );
+    games.Add(game);
+    return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id}, game);
+});
+
+// PUT /games/id
+app.MapPut("/games/{id}", (int id , UpdateGameDto updatedGame) =>
+{
+    var index = games.FindIndex(game => game.Id == id);
+
+    games[index] = new GameDto(
+        id,
+        updatedGame.Name,
+        updatedGame.Genre,
+        updatedGame.Price,
+        updatedGame.ReleaseDate
+    );
+
+    return Results.NoContent();
+} );
+
+// DELETE /games/id
+app.MapDelete("/games/{id}", (int id) =>
+{
+    games.RemoveAll(game => game.Id == id);
+    return Results.NoContent();
+});
+
 
 app.Run();
