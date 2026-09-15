121f9edaddb7603aff71c50cec452c54ebfb8a13
maherhms <maher.3000@outlook.fr>
2026-09-15 09:59:40 +0200

    Implement data migration and auto population of dbset Genre and auto db update on run

--- GameStore.Api/Data/GameStoreContext.cs
+++ GameStore.Api/Data/GameStoreContext.cs
@@ -1,4 +1,5 @@
 using System;
+using GameStore.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,15 +1,12 @@
-using GameStore.Api;
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
-
-var connString = "Data Source=GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(connString);
-
+builder.AddGameStoreDb();
 var app = builder.Build();
 app.MapGamesEndpoints();
+app.MigrateDb();
 
 app.Run();
